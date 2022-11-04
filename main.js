3e6f3220bc5246f803a06c130e33470e0c46ef16
unknown <dgoodenough@proton.me>
2022-11-04 09:43:36 -0700

    get search value and start to layout fetch and adduser function:

--- main.js
+++ main.js
@@ -2,6 +2,29 @@
 
 let users = [];
 
+function addUser (user) {
+  //add user to users array
+  //if array is certain length cut some off the end
+}
+
+function fetch(query) {//finish ajac
+  $.ajax({
+    method: "GET",
+    success: function (data) {
+      addUser(data);
+    },
+    error: function() {
+
+    }
+  })
+}
+
+$('#search').click(function () {
+  let searchVal = $('#search-bar').val();
+  
+  fetch(searchVal);
+})
+
 function renderUsers() {
   let source = $('#user-template').html();
 
